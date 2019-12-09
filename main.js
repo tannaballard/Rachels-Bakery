d7413d0aaf29fde8d7b6e539ac1b2938afc1f1de
tanna ballard <53823038+tannaballard@users.noreply.github.com>
2019-12-08 21:38:32 -0600

    Update main.js

--- main.js
+++ main.js
@@ -21,16 +21,34 @@ function validateForm() {
     return isEmpty(email) || re.test(email);
     }
 // this is for catering
-    function update() { 
-        var element = document.getElementById("myprogressBar");    
-        var width = 1; 
-        var identity = setInterval(scene, 10); 
-        function scene() { 
-          if (width >= 100) { 
-            clearInterval(identity); 
-          } else { 
-            width++;  
-            element.style.width = width + '%';  
-          } 
-        } 
-      } 
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
